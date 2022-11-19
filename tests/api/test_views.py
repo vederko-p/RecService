d6abeff7c3310bb3d2e6b265ed12965b4861622e
Skibin Maksim <ski6a_23@mail.ru>
2022-11-19 17:55:45 +0300

    test views: add unknow model test

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -34,12 +34,18 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="test_model", user_id=user_id)
     with client:
         response = client.get(path)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
 
 
-def test_get_reco_for_unknown_model():
-    pass
+def test_get_reco_for_unknown_model(
+    client: TestClient,
+) -> None:
+    user_id = 0
+    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    with client:
+        response = client.get(path)
+    assert response.status_code == 404
