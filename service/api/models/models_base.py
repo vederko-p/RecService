d0f792a75f916953a0287708879b01b0f2d00b81
Skibin Maksim <ski6a_23@mail.ru>
2022-11-19 17:08:42 +0300

    models base: split check model and init model logic

--- service/api/models/models_base.py
+++ service/api/models/models_base.py
@@ -1,16 +1,22 @@
 
-from typing import List
+from typing import List as _List
 
 from service.api.models.base_model import BaseModel as _BaseModel
 from service.api.models.test_model import TestModel
 
 
 class ModelsBase:
-    def __init__(self, models: List[_BaseModel]):
+    def __init__(self, models: _List[_BaseModel]):
         self.models = {m.model_name: m for m in models}
 
-    def get_model(self, model_name: str) -> _BaseModel:
-        return self.models.get(model_name)
+    def check_model(self, model_name: str) -> bool:
+        return self.models.get(model_name) is not None
+
+
+    def init_model(self,  model_name: str) -> _BaseModel:
+        model_link = self.models.get(model_name)
+        model = model_link()
+        return model
 
 
 AVAILABLE_MODELS = [
