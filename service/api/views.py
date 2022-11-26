779abfbbec1bfeac2e4aec55d5f7d812f2180ab4
Skibin Maksim <ski6a_23@mail.ru>
2022-11-26 15:21:29 +0300

    views models: refactor due to security

--- service/api/views.py
+++ service/api/views.py
@@ -1,13 +1,14 @@
 from typing import List, Union
 
 from fastapi import APIRouter, FastAPI, Request, Depends
-from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
 from service.api.exceptions import UserNotFoundError, ModelNotFoundError
 from service.log import app_logger
 
 from service.api.models.models_base import models_base
+
+from service.api.secure_token import BotRequest, get_bot_request
 
 
 class RecoResponse(BaseModel):
@@ -32,25 +33,31 @@ async def health() -> str:
     response_model=RecoResponse,
 )
 async def get_reco(
-    request: Request,
-    model_name: str,
-    user_id: int,
+    bot_request: BotRequest = Depends(get_bot_request)
 ) -> RecoResponse:
-    app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
+    app_logger.info(
+        f"Request for model: {bot_request.model_name}, user_id: {bot_request.user_id}"
+    )
 
-    if user_id > 10**9:
-        raise UserNotFoundError(error_message=f"User {user_id} not found")
+    print()
+    print('bot_request:')
+    print(bot_request)
+    print()
 
-    if models_base.check_model(model_name):
-        model = models_base.init_model(model_name)
+    if bot_request.user_id > 10**9:
+        raise UserNotFoundError(
+                error_message=f"User {bot_request.user_id} not found")
+
+    if models_base.check_model(bot_request.model_name):
+        model = models_base.init_model(bot_request.model_name)
     else:
         raise ModelNotFoundError(
-            status_code=404, detail=f'Model {model_name} not found')
+            status_code=404, detail=f'Model {bot_request.model_name} not found')
             # TODO: Use status.HTTP_404_NOT_FOUND instead of 404
             #       (from fastapi import status)
     
-    reco = model.predict(request.app.state.k_recs)
-    return RecoResponse(user_id=user_id, items=reco)
+    reco = model.predict(bot_request.k_recs)
+    return RecoResponse(user_id=bot_request.user_id, items=reco)
 
 
 def add_views(app: FastAPI) -> None:
