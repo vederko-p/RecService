d39dfd177462d7106f4890d805fa28a82f59cb41
Skibin Maksim <ski6a_23@mail.ru>
2022-11-26 18:23:08 +0300

    security: fix check token + chage tokens list to one token

--- service/api/secure_token.py
+++ service/api/secure_token.py
@@ -7,9 +7,7 @@ from pydantic import BaseModel
 from passlib.context import CryptContext
 
 
-AVAILABLE_HASHES = [
-    '$2b$12$uee7sjovrt.Pwxu1HR487ek7YzZjFh5XOk1fYau5CirLNP3gOWhI.'
-]
+AVAILABLE_HASH = '$2b$12$uee7sjovrt.Pwxu1HR487ek7YzZjFh5XOk1fYau5CirLNP3gOWhI.'
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -22,9 +20,8 @@ class BotRequest(BaseModel):
     k_recs: int
 
 
-async def check_token(token, available_tokens: List[str]):
-    token_hash = pwd_context.hash(token)
-    if token_hash not in available_tokens:
+async def check_token(token: str):
+    if not pwd_context.verify(token, AVAILABLE_HASH):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate token",
@@ -42,7 +39,7 @@ async def get_bot_request(
     k_items: int = Depends(get_k_itmes),
     token: str = Depends(oauth2_scheme)
 ) -> BotRequest:
-    check_token(token, AVAILABLE_HASHES)
+    await check_token(token)
     bot_request = BotRequest(
             model_name=model_name, user_id=user_id, k_recs=k_items)
     return bot_request
