302af419ed73eddf79d72670d5d70e413f357595
Skibin Maksim <ski6a_23@mail.ru>
2022-12-06 10:05:43 +0300

    fix: abstract class implementation

--- service/api/models/base_model.py
+++ service/api/models/base_model.py
@@ -1,8 +1,12 @@
 
-class BaseModel:
+from abc import ABC, abstractmethod
+
+
+class BaseModel(ABC):
     def __init__(self, model_name: str):
         self.model_name = model_name
     
+    @abstractmethod
     def predict(self):
         pass
     
