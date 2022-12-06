ccfb9ec7c3a55086d57468d205b0b1a0ba5d7191
Skibin Maksim <ski6a_23@mail.ru>
2022-12-06 22:51:24 +0300

    models: fix typings

--- service/api/models/models_base.py
+++ service/api/models/models_base.py
@@ -1,19 +1,19 @@
 
-from typing import List as _List
+from typing import List
 
-from service.api.models.base_model import BaseModel as _BaseModel
+from service.api.models.base_model import BaseModel
 from service.api.models.test_model import TestModel
 
 
 class ModelsBase:
-    def __init__(self, models: _List[_BaseModel]):
+    def __init__(self, models: List[BaseModel]):
         self.models = {m.model_name: m for m in models}
 
     def check_model(self, model_name: str) -> bool:
         return self.models.get(model_name) is not None
 
 
-    def init_model(self,  model_name: str) -> _BaseModel:
+    def init_model(self,  model_name: str) -> BaseModel:
         model_link = self.models.get(model_name)
         model = model_link()
         return model
