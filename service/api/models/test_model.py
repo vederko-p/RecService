919ff732a29881cfc167d375624c6f448e88e6d2
Skibin Maksim <ski6a_23@mail.ru>
2022-12-06 16:40:29 +0300

    reformat: delete underscore

--- service/api/models/test_model.py
+++ service/api/models/test_model.py
@@ -1,14 +1,14 @@
 
-from typing import List as _List
+from typing import List
 
-from service.api.models.base_model import BaseModel as _BaseModel
+from service.api.models.base_model import BaseModel
 
 
-class TestModel(_BaseModel):
+class TestModel(BaseModel):
     model_name = 'test_model'
 
     def __init__(self):
         super().__init__(self.model_name)
 
-    def predict(self, user_id: int, k:int) -> _List[int]:
+    def predict(self, user_id: int, k:int) -> List[int]:
         return list(range(k))
