4899c81935fa3e9ab0ec1afb96c9e4614771ff59
Skibin Maksim <ski6a_23@mail.ru>
2022-12-09 01:48:32 +0300

    pre-commit: all files

--- service/api/models/knn_model.py
+++ service/api/models/knn_model.py
@@ -8,8 +8,8 @@ from service.log import app_logger
 
 
 class KNNModel(BaseModel):
-    model_name = 'knn_model'
-    
+    model_name = "knn_model"
+
     def __init__(
         self,
         user_segment_map: dict,
@@ -20,17 +20,17 @@ class KNNModel(BaseModel):
         self.user_segment_map = user_segment_map
         self.segment_model_map = segment_model_map
         self.pop_items = pop_items
-        
-    def predict(self, user_id: int, k:int) -> List[int]:
+
+    def predict(self, user_id: int, k: int) -> List[int]:
         user_segment = self.user_segment_map.get(user_id)
         if user_segment is None:
             return self._predict_popular(k)
         else:
             return self._predict_by_model(user_id, user_segment, k)
-        
+
     def _predict_popular(self, k: int) -> List[int]:
         return self.pop_items[:k].tolist()
-        
+
     def _get_similar_users(
         self,
         user_id: int,
@@ -40,75 +40,70 @@ class KNNModel(BaseModel):
         model = self.segment_model_map[user_segment]
         # find similar users:
         inner_user_id = model.users_mapping[user_id]
-        sim = list(filter(
-            lambda x: x[1] < 1,
-            list(zip(
-                *model.user_knn.similar_items(
-                    inner_user_id,
-                    N=model.N_users
+        sim = list(
+            filter(
+                lambda x: x[1] < 1,
+                list(
+                    zip(
+                        *model.user_knn.similar_items(
+                            inner_user_id, N=model.N_users
+                        )
                     )
-            ))[1:] # exclude same user
-        ))
+                )[
+                    1:
+                ],  # exclude same user
+            )
+        )
         return (
             [model.users_inv_mapping[user] for user, _ in sim],
-            [user_s for _, user_s in sim]
+            [user_s for _, user_s in sim],
         )
-        
+
     def _predict_by_model(
-        self,
-        user_id: int,
-        user_segment: int,
-        k: int
+        self, user_id: int, user_segment: int, k: int
     ) -> List[int]:
         # get similar users (ids and scores):
-        sim_users, sim_score = self._get_similar_users(
-            user_id, user_segment
-        )
+        sim_users, sim_score = self._get_similar_users(user_id, user_segment)
         # prepare df to sort items of similar users:
         model = self.segment_model_map[user_segment]
         sim_users_items = model.watched.loc[sim_users]
-        sim_users_items['sim'] = sim_score
+        sim_users_items["sim"] = sim_score
         # evaluate score:
         sim_users_items = (
             # items of similar users:
-            sim_users_items
-            .explode('item_id')
+            sim_users_items.explode("item_id")
             .reset_index()
-            .rename(columns={'user_id': 'sim_user_id'})
-            .drop_duplicates(['item_id'],keep='first')
+            .rename(columns={"user_id": "sim_user_id"})
+            .drop_duplicates(["item_id"], keep="first")
             # idf of items of similar users:
             .merge(
-                model.item_idf,
-                left_on='item_id',
-                right_on='index',
-                how='left'
+                model.item_idf, left_on="item_id", right_on="index", how="left"
             )
         )
         # final score:
-        sim_users_items['score'] = (
-            sim_users_items['sim'] * sim_users_items['idf']
+        sim_users_items["score"] = (
+            sim_users_items["sim"] * sim_users_items["idf"]
         )
         # sort by score:
-        sim_users_items = (
-            sim_users_items.sort_values(['score'], ascending=False)
+        sim_users_items = sim_users_items.sort_values(
+            ["score"], ascending=False
         )
-        recs = sim_users_items['item_id'].iloc[:k].tolist()
+        recs = sim_users_items["item_id"].iloc[:k].tolist()
         # complete with popular:
         if len(recs) < k:
-            pop = list(filter(
-                lambda x: x not in recs,
-                self._predict_popular(k)
-            ))
+            pop = list(
+                filter(lambda x: x not in recs, self._predict_popular(k))
+            )
             recs = (recs + pop)[:k]
         return recs
-    
+
     def warmup(self, users_ids: List[int]) -> None:
         for user_id in users_ids:
             self.predict(user_id, k=10)
 
 
 def read_dill(filepath: str):
-    with open(filepath, 'rb') as f:
+    with open(filepath, "rb") as f:
         data = dill.load(f)
     return data
 
@@ -119,47 +114,39 @@ class KNNModelConfig:
         users_segment_map_path: str,
         sub_estimators_path: str,
         items_pop_ordered_path: str,
-        warmup_users_path: str = None
+        warmup_users_path: str = None,
     ):
         self.users_segment_map_path = users_segment_map_path
         self.sub_estimators_path = sub_estimators_path
         self.items_pop_ordered_path = items_pop_ordered_path
         self.warmup_users_path = warmup_users_path
-        
-        self.users_segment_map = None,
-        self.sub_estimators = None,
+
+        self.users_segment_map = (None,)
+        self.sub_estimators = (None,)
         self.items_pop_ordered = None
         self.warmup_users = None
-    
-    def parse(self):
-        self.users_segment_map = read_dill(
-            self.users_segment_map_path
-        )
-        self.sub_estimators = read_dill(
-            self.sub_estimators_path
-        )
-        self.items_pop_ordered = read_dill(
-            self.items_pop_ordered_path
-        )
-        if self.warmup_users_path is not None:
-            self.warmup_users = read_dill(
-                self.warmup_users_path
-            )
 
-            
+    def parse(self):
+        self.users_segment_map = read_dill(self.users_segment_map_path)
+        self.sub_estimators = read_dill(self.sub_estimators_path)
+        self.items_pop_ordered = read_dill(self.items_pop_ordered_path)
+        if self.warmup_users_path is not None:
+            self.warmup_users = read_dill(self.warmup_users_path)
+
+
 class KNNModelInitializer:
     def __init__(
         self,
         config: KNNModelConfig,
     ):
         self.config = config
-        
+
     def init_model(self):
         self.config.parse()
         model = KNNModel(
             user_segment_map=self.config.users_segment_map,
             segment_model_map=self.config.sub_estimators,
-            pop_items=self.config.items_pop_ordered
+            pop_items=self.config.items_pop_ordered,
         )
         if self.config.warmup_users is not None:
             model.warmup(self.config.warmup_users)
@@ -167,10 +154,10 @@ class KNNModelInitializer:
 
 
 knn_model_config = KNNModelConfig(
-    users_segment_map_path = 'service/api/models/files/users_segment_map.dill',
-    sub_estimators_path = 'service/api/models/files/segment_model_map.dill',
-    items_pop_ordered_path = 'service/api/models/files/items_pop_ordered.dill',
-    warmup_users_path = None  # 'files/warmup_users.dill' 
+    users_segment_map_path="service/api/models/files/users_segment_map.dill",
+    sub_estimators_path="service/api/models/files/segment_model_map.dill",
+    items_pop_ordered_path="service/api/models/files/items_pop_ordered.dill",
+    warmup_users_path=None,  # 'files/warmup_users.dill'
 )
 
 knn_model_initializer = KNNModelInitializer(knn_model_config)
